ce05847e5f49a056e383210df5980ea443d8003c
Carlos Rivera <rivera.utx@gmail.com>
2011-12-26 15:16:58 -0600

    added django forms

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from kaw.main.forms import ContactForm
 
 def index(request):
     d = {}
+    d['contact_form'] = ContactForm()
     return render_to_response('index.html', d, context_instance=RequestContext(request))
 
