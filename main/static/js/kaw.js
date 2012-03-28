af127bc0313db66fce39f4bbb3d4973f6f674177
Carlos Rivera <rivera.utx@gmail.com>
2012-03-28 00:12:30 -0500

    fixed textarea

--- main/static/js/kaw.js
+++ main/static/js/kaw.js
@@ -3,7 +3,7 @@ $(function () {
         var input_name = $('input[name=name]').val();
         var input_email = $('input[name=email]').val();
         var input_subject = $('input[name=subject]').val();
-        var input_message = $('input[name=message]').val();
+        var input_message = $('#contact-message').val();
         var response_text = $('#response');
         var csrf = $('input[name=csrfmiddlewaretoken]').val();
         $.post('/cform_submit/', {name:input_name, email:input_email, subject:input_subject, message:input_message, csrfmiddlewaretoken: csrf}, function (data) {
