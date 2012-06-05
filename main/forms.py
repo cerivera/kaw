4a88f12c137102f489419ced054ea9a3045f7ec1
Carlos Rivera <rivera.utx@gmail.com>
2012-06-05 08:55:02 -0500

    removed subject

--- main/forms.py
+++ main/forms.py
@@ -3,5 +3,4 @@ from django import forms
 class ContactForm(forms.Form):
     name = forms.CharField()
     email = forms.CharField()
-    subject = forms.CharField()
     message = forms.CharField(widget=forms.Textarea)
