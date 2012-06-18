138debdf5bfcca2143c0d1d65b6029a915bec311
Carlos Rivera <rivera.utx@gmail.com>
2012-06-17 23:36:12 -0500

    new models

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,9 @@
 __author__ = 'Carlos'
 
-from kaw.main.models import Client
+from kaw.main.models import Client, Address, Pet
 from django.contrib import admin
 
 admin.site.register(Client)
+admin.site.register(Address)
+admin.site.register(Pet)
+
