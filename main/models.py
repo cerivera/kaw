c778c37a739bb18ec54d980be720ee083a1880da
Carlos Rivera <rivera.utx@gmail.com>
2012-06-17 23:38:30 -0500

    removed fields from address

--- main/models.py
+++ main/models.py
@@ -10,9 +10,6 @@ class Address(models.Model):
 
     type = models.CharField(_('Type'), max_length=20, choices = TYPES_CHOICES)
 
-    first_name = models.CharField(_('First name'), max_length = 50, blank = True)
-    last_name = models.CharField(_('Last name'), max_length = 50, blank = True)
-    phone_number = models.CharField(_('Phone number'), max_length = 30, blank = True)
     street1 = models.CharField(_('Address 1'), max_length = 100, blank = True)
     street2 = models.CharField(_('Address 2'), max_length = 100, blank = True)
     zipcode = models.CharField(_('ZIP code'), max_length = 5, blank = True)
@@ -31,7 +28,9 @@ class Pet(models.Model):
     note = models.TextField(_('Notes'))
 
 class Client(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(_('First name'), max_length=30)
+    last_name = models.CharField(_('Last name'), max_length=30)
+    email = models.EmailField(_('Email'))
+    phone_number = models.CharField(_('Phone number'), max_length = 30, blank = True)
     address = models.ForeignKey(Address)
     pet = models.ForeignKey(Pet)
