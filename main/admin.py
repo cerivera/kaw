a9ebe3e35e8d11bbf2f1c2ff9401e4ccc083f123
Carlos Rivera <rivera.utx@gmail.com>
2012-06-17 23:45:50 -0500

    modifying admin lists

--- main/admin.py
+++ main/admin.py
@@ -3,7 +3,12 @@ __author__ = 'Carlos'
 from kaw.main.models import Client, Address, Pet
 from django.contrib import admin
 
-admin.site.register(Client)
-admin.site.register(Address)
-admin.site.register(Pet)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+class PetAdmin(admin.ModelAdmin):
+    list_display = ('name')
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ('type')
+
+admin.site.register(Client, ClientAdmin, Address, AddressAdmin, Pet, PetAdmin)
 
